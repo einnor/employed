e635c2a60cfdc27de9590f6d048a5b25f9b33bb6
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-15 21:43:09 +0300

    Add router middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const apiRoutes = require('./routes');
+
 const PORT = 8080;
 const app = express();
 
 app.use(express.json());
+
+apiRoutes.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ data: 'Up and running!' });
