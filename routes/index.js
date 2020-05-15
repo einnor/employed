67fa83e4ee1de576c479b5df81bef524dc89605f
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-15 22:48:10 +0300

    post /contacts route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
+const controller = require('../controllers/employee.controller');
+
 router.get('/', (req, res) => {
   res.json({
     status: 'Api is working',
     message: 'Welcome to the employee api router',
   });
 });
+
+router.post('/contacts', controller.createEmployee);
 
 module.exports = router;
