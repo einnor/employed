26f1e08a42c2669c2791cb7e0bced821a750f8d0
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-15 23:00:18 +0300

    Validations using joi

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,7 +1,20 @@
 const employeeModel = require('../models/employee.model');
+const joi = require('@hapi/joi');
+
+const schema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().required().email(),
+  password: joi.string().min(6).required(),
+  gender: joi.string(),
+  phone: joi.string(),
+});
 
 const createEmployee = async (req, res, next) => {
   try {
+    const joiCheck = schema.validate(req.body);
+    if (joiCheck.error) {
+      return res.status(400).json(joiCheck.error);
+    }
     const newEmployee = await employeeModel.create(req.body);
     res.status(201).json(newEmployee);
   } catch (error) {
