4bea24b900530ad19ea3c33edd06b596e78a456b
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-18 11:57:39 +0300

    Mock return value of model.findById

--- test/unit/controller.getEmployeeById.test.js
+++ test/unit/controller.getEmployeeById.test.js
@@ -6,11 +6,18 @@ const mockEmployees = require('../mockdata/employees.json');
 
 model.findById = jest.fn();
 
+let req, res, next;
+req = httpMock.createRequest();
+res = httpMock.createResponse();
+next = null;
+
 describe('Get Employee By ID - Controller', () => {
   test('getEmployeeById function is defined', () => {
     expect(typeof getEmployeeById).toBe('function');
   });
 
-  test('return an employee by id', () => {
+  test('return an employee by id', (req, res, next) => {
+    req.params.id = mockEmployees[0]._id;
+    model.findById.mockReturnValue(mockEmployees[0]);
   });
 });
