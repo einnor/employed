95ef99ca70f5333ed704339b51f5ff94552db122
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-18 11:54:01 +0300

    Stub model.findById

--- test/unit/controller.getEmployeeById.test.js
+++ test/unit/controller.getEmployeeById.test.js
@@ -1,8 +1,16 @@
+const httpMock = require('node-mocks-http');
+
 const { getEmployeeById } = require('../../controllers/employee.controller');
 const model = require('../../models/employee.model');
+const mockEmployees = require('../mockdata/employees.json');
+
+model.findById = jest.fn();
 
 describe('Get Employee By ID - Controller', () => {
   test('getEmployeeById function is defined', () => {
     expect(typeof getEmployeeById).toBe('function');
+  });
+
+  test('return an employee by id', () => {
   });
 });
