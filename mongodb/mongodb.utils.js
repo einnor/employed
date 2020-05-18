4cad8f30d8f2628f8dd1f28f86aa0aba2d267ff6
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-18 11:15:40 +0300

    Cleaning up

--- mongodb/mongodb.utils.js
+++ mongodb/mongodb.utils.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URL = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0-sl3oo.mongodb.net/test?retryWrites=true&w=majority`;
+const URL = process.env.MONGO_URL;
 
 const connect = async () => {
   try {
