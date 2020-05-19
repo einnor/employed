8c5e650cd420cbf38012f4b62ada525c4b9857bc
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-19 11:59:38 +0300

    Initialize test variables

---
+++ test/unit/controller.createEmployee.test.js
@@ -0,0 +1,22 @@
+const httpMock = require('node-mocks-http');
+
+const { createEmployee } = require('../../controllers/employee.controller');
+const model = require('../../models/employee.model');
+const mockEmployees = require('../mockdata/employees.json');
+const reqPayload = require('../mockdata/employeeReqPayload.json');
+
+model.create = jest.fn();
+
+let req, res, next;
+
+describe('Create Employee - Controller', () => {
+  beforeEach(() => {
+    req = httpMock.createRequest();
+    res = httpMock.createResponse();
+    next = null;
+  });
+
+  afterEach(() => {
+    model.create.mockClear();
+  });
+});
