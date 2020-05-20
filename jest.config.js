23c5905b888a6bd557d32bc50c4c162f0d713865
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 13:40:25 +0300

    Test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   testEnvironment: 'node',
   testTimeout: 30000,
+  reporters: [
+    'default',
+    [
+      './node_modules/jest-html-reporter',
+      {
+        pageTitle: 'Test-Report',
+        outputPath: 'Test-Report.html',
+        includeFailureMsg: true,
+      },
+    ],
+  ],
 };
