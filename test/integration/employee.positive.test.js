a8ed9a22f62bbb1cceabd677b7b25ac0accad625
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-05-20 11:40:38 +0300

    Integration Test - GET /api/contacts/:id

--- test/integration/employee.positive.test.js
+++ test/integration/employee.positive.test.js
@@ -32,4 +32,24 @@ describe('Positive Scenarios - Integration Tests', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBeGreaterThan(0);
   });
+
+  test.only('GET /api/contacts/:id', async () => {
+    const responseOfCreate = await request(app)
+      .post(contactsURL)
+      .send({
+        name: 'Jane Doe',
+        email: 'jane.doe@example.com',
+        password: 'password',
+      });
+    expect(responseOfCreate.statusCode).toBe(201);
+
+    const response = await request(app)
+      .get(`${contactsURL}/${responseOfCreate.body._id}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('name', responseOfCreate.body.name);
+    expect(response.body).toHaveProperty('email', responseOfCreate.body.email);
+    expect(response.body).toHaveProperty('_id', responseOfCreate.body._id);
+    expect(response.body).toHaveProperty('password', responseOfCreate.body.password);
+    expect(response.body).toHaveProperty('createdAt', responseOfCreate.body.createdAt);
+  });
 });
